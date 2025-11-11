3db2c21dfd64f6fb4965c69175f8ff49b1fec205
Philpax <me@philpax.me>
2025-11-11 02:54:33 +0100

    feat(gi): move defs out to pyxis-defs

--- jc3gi/build.rs
+++ jc3gi/build.rs
@@ -1,3 +1,9 @@
+use std::path::Path;
+
 fn main() {
-    pyxis::build_script(Some(std::path::Path::new("src"))).unwrap();
+    pyxis::build_script(
+        Path::new("pyxis-defs/JustCause3/Steam/1227440"),
+        Some(Path::new("src")),
+    )
+    .unwrap();
 }
